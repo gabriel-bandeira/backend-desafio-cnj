a97c29bc194d69693f029509fbd22382cacfc83b
Luiz Felipe Vercosa <lfv.vercosa@gmail.com>
2020-10-20 21:13:16 -0300

    fix: unlimit number of connections postgree

--- panorama/settings.py
+++ panorama/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'PASSWORD': 'panorama',
         'HOST': 'localhost',
         'PORT': '5432',
-        'CONN_MAX_AGE': 500                                                               }
+        'CONN_MAX_AGE': None                                                               }
  }
 
 # Password validation
