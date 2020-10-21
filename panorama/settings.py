9b49bb44d3a3d84877755f60d205cf5a7f99cd85
Luiz Felipe Vercosa <lfv.vercosa@gmail.com>
2020-10-21 12:07:14 -0300

    fix: avoid keep connection alive in database

--- panorama/settings.py
+++ panorama/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'PASSWORD': 'panorama',
         'HOST': 'localhost',
         'PORT': '5432',
-        'CONN_MAX_AGE': None                                                               }
+        'CONN_MAX_AGE': 0                                                               }
  }
 
 # Password validation
